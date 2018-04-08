1d4c6de83c4c32bcc4dfd81bfee7fc229c76e638
Jurguen <jurguen.r.r@gmail.com>
2018-04-08 16:45:17 -0600

    Add files via upload

---
+++ Othello/Othello/Fichas.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace Othello
+{
+    class Fichas
+    {
+        public static int[,] tablero;
+
+
+        public Fichas(int[,] tableroJugada, int tamTablero)
+        {
+            tablero = tableroJugada;
+        }
+
+        /// <summary>
+        /// Función encargada de obtener las posiciones para comprobar si es necesario cambiar las fichas de color, recibe las posiciones                         
+        /// que va a validar y el tamaño del tablero
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="tamTablero"></param>
+        /// <param opt="opcion 0(recorrido) o 1(contar)"></param>
+        /// <returns></returns>
+        
+        static int cantG=0;
+        public static int posXG;
+        public static int posYG;
+        public static int[,] Comprobacion(int x, int y,int color, int tamTablero,int opt) { 
+            int cantidad=0;
+           
+            if ((x != 0) && (tablero[x, y] != tablero[x - 1, y] ) ){ //1    
+                //Computadora, recorre y cambia de color
+                if(opt==0){
+                    if(tablero[x - 1, y]!=2){
+
+                        Recorrido(x, y, tablero[x, y], -1, 0,tamTablero,opt);
+                    }
+                }
+                //Persona, recorre y cambia color de fichas
+                else if(opt==3){
+                    RecorridoPersona(x,y,color,-1,0);
+                }
+                //cuenta para mejor movimiento
+                else{
+                    cantidad=cantidad+Recorrido(x, y, color, -1, 0,tamTablero,opt);
+                }
+            }   
+
+            if ((y != tamTablero - 1)&&(x!=0) && (tablero[x, y] != tablero[x - 1, y + 1]) )//2
+            {
+                //Computadora, recorre y cambia de color
+                if(opt==0){
+                    if(tablero[x - 1, y + 1] != 2 ){
+                        Recorrido(x, y, tablero[x, y], -1, 1, tamTablero,opt);
+                    }
+                }
+                //Persona, recorre y cambia color de fichas
+                else if(opt==3){
+                    RecorridoPersona(x,y,color,-1,1);
+                }
+                //cuenta para mejor movimiento
+                else{
+                    cantidad=cantidad+Recorrido(x, y, color, -1, 1,tamTablero,opt);
+                }
+                
+            }
+            if ((y != tamTablero - 1) && (tablero[x, y] != tablero[x, y + 1]))//3
+            {  
+                //Computadora, recorre y cambia de color
+                if(opt==0){
+                    if(tablero[x, y + 1] != 2){
+                        Recorrido(x, y, tablero[x, y], 0, 1, tamTablero,opt);
+                    }
+                }
+                //Persona, recorre y cambia color de fichas
+                else if(opt==3){
+                    RecorridoPersona(x,y,color,0,1);
+                }
+                //cuenta para mejor movimiento
+                else{
+                    cantidad=cantidad+Recorrido(x, y, color, 0, 1,tamTablero,opt);
+                }
+
+            }
+            if ((y != tamTablero - 1) && (x!=tamTablero-1) && (tablero[x, y] != tablero[x + 1, y + 1]))//4
+            {   
+                //Computadora, recorre y cambia de color
+                if(opt==0){
+                    if(tablero[x + 1, y + 1] != 2){
+                        Recorrido(x, y, tablero[x, y], 1, 1, tamTablero,opt);
+                    }
+                }
+                //Persona, recorre y cambia color de fichas
+                else if(opt==3){
+                    RecorridoPersona(x,y,color,1,1);
+                }
+                //cuenta para mejor movimiento
+                else{
+                    cantidad=cantidad+Recorrido(x, y, color, 1, 1,tamTablero,opt);
+                }
+
+            }
+            if ((x != tamTablero - 1) && (tablero[x, y] != tablero[x + 1, y]) )//5
+            {   
+                //Computadora, recorre y cambia de color
+                if(opt==0){
+                    if(tablero[x + 1, y] != 2){
+                        Recorrido(x, y, tablero[x, y], 1, 0, tamTablero,opt);
+                    }
+                }
+                //Persona, recorre y cambia color de fichas
+                else if(opt==3){
+                    RecorridoPersona(x,y,color,1,0);
+                }
+                //cuenta para mejor movimiento
+                else{
+                    cantidad=cantidad+Recorrido(x, y, color, 1, 0,tamTablero,opt);
+                }
+            }
+            if ((x != tamTablero-1)&&( y!=0) && (tablero[x, y] != tablero[x + 1, y - 1]))//6
+            {
+                //Computadora, recorre y cambia de color
+                if(opt==0){
+                    if(tablero[x + 1, y - 1] != 2){
+                        Recorrido(x, y, tablero[x, y], 1, -1, tamTablero,opt);
+                    }
+                }
+                //Persona, recorre y cambia color de fichas
+                else if(opt==3){
+                    RecorridoPersona(x,y,color,1,-1);
+                }
+                //cuenta para mejor movimiento
+                else{
+                    cantidad=cantidad+Recorrido(x, y, color, 1, -1,tamTablero,opt);
+                }
+            }
+            if ((y != 0) && (tablero[x, y] != tablero[x, y - 1]))//7
+            {   
+                //Computadora, recorre y cambia de color
+                if(opt==0){
+                    if(tablero[x, y - 1] != 2){
+                        Recorrido(x, y, tablero[x, y],0, -1, tamTablero,opt);
+                    }
+                }
+                //Persona, recorre y cambia color de fichas
+                else if(opt==3){
+                    RecorridoPersona(x,y,color,0,-1);
+                }
+                //cuenta para mejor movimiento                
+                else{
+                    cantidad=cantidad+Recorrido(x, y, color, 0,-1,tamTablero,opt);
+                }
+            }
+            if ((x != 0)&&(y!=0) && (tablero[x, y] != tablero[x -1, y - 1]))//8
+            {   
+                //Computadora, recorre y cambia de color
+                if(opt==0){
+                    if(tablero[x -1, y - 1] != 2 ){
+                        Recorrido(x, y, tablero[x, y], -1, -1, tamTablero,opt);
+                    }
+                }
+                //Persona, recorre y cambia color de fichas
+                else if(opt==3){
+                    RecorridoPersona(x,y,color,-1,-1);
+                }
+                //cuenta para mejor movimiento
+                else{
+                    cantidad=cantidad+Recorrido(x, y, color, -1, -1,tamTablero,opt);
+                }
+
+            }
+            //Console.WriteLine(cantidad + "Cantidad"+opt);
+            
+            if (cantG<cantidad & opt==1){
+                Console.WriteLine(x+" x , y "+y);
+                Console.WriteLine(tablero[x,y]);
+                tablero[x,y]=3;
+                cantG=cantidad;
+                posXG=x;
+                posYG=y;
+                //esto se debe guardar para enviarlo como mejor jugada despu'es de revisar todas
+                
+                }
+            
+            return tablero;
+        }
+
+
+        /// <summary>
+        /// Función encargada de revisar si la dirección que recibe tiene la posibilidad de cambiar las fichas esto la computadora. Recibe las posiciones de la matriz, el color al que debe cambiar  
+        /// y sumaX, sumaY que significan la dirección en que se debe mover en la matriz                                                               
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="color"></param>
+        /// <param name="sumaX"></param>
+        /// <param name="sumaY"></param>
+        /// <param name="t"></param>
+        /// <param name="opt"></param>
+        /// <returns></returns>
+        public static int Recorrido(int x, int y, int color, int sumaX, int sumaY, int t,int opt) {  
+                                                                                      
+            int i = x + sumaX;                                                        
+            int j = y + sumaY;                                                        
+            int cantidad = 0;                                                         
+            if(opt==0)
+            {
+                while (tablero[i,j] != color)
+                {
+
+                    if (tablero[i, j] == 2)
+                    {
+                        //manda a comprobar la posicion [i,j]
+                        //necesito guardar la posicion para cambiar si es buena.
+                        
+                        Comprobacion(i, j,color, t,opt+1);
+                        //cambiarFichas(x, y, i, j, color, sumaX, sumaY);
+                        return 0;
+                    }
+                    else {
+                        i = i + sumaX;
+                        j = j + sumaY;
+                    }
+                }
+                return 0;
+            }
+            else{
+
+                while (tablero[i,j] != 2)
+                {
+                    if (tablero[i, j] == color)
+                    {
+                        return cantidad;
+                        
+                    }
+                    else {
+                        i = i + sumaX;
+                        j = j + sumaY;
+                        cantidad++;
+                    }
+                }
+                return 0;
+            }
+
+        }
+
+        
+        /// <summary>
+        /// Función que se encarga de cambiar el color de la fichas a nivel lógico 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="color"></param>
+        /// <param name="tamTablero"></param>
+        /// <returns></returns>
+         public static void RecorridoPersona(int x, int y, int color, int sumaX, int sumaY) {  
+                                                                                      
+            int i = x + sumaX;                                                        
+            int j = y + sumaY;                                                        
+            
+            while (tablero[i,j] != 2)
+            {
+                if (tablero[i, j] == color)
+                {
+                    cambiarFichas(x, y, color, sumaX, sumaY);
+                    break;
+                }
+                else {
+                    i = i + sumaX;
+                    j = j + sumaY;
+                    
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Función que se encarga de cambiar el color de la fichas a nivel lógico 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        /// <param name="color"></param>
+        /// <param name="tamTablero"></param>
+        /// <returns></returns>
+
+        public static void cambiarFichas(int x, int  y,int color, int sumaX, int sumaY) {
+            
+            x = x + sumaX;
+            y = y + sumaY;
+
+            while (tablero[x,y]!=color) {
+                tablero[x, y] = color;
+                x = x + sumaX;
+                y = y + sumaY;
+            }
+        }
+    }
+}
---
+++ Othello/Othello/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Othello
+{
+    class Program
+    {
+        private static int[,] tablero;
+
+
+        /// <summary>
+        /// Función que va a rellena la matriz lógica con el tamaño de tablero elegido por el usuario.
+        /// Utiliza los números 1 = blanco, 0 = negro, 2 = espacio vacío
+        /// </summary>
+        /// <param name="tamTablero"></param>
+        /// <returns></returns>
+        
+        public static void RellenarTablero(int tamTablero)
+        {
+            tablero=new int[tamTablero,tamTablero];
+            /*tablero = new int[8, 8]{{   2,2,2,2,2,2,2 },
+                                    { 2,2,1,2,2,2,2,2 },
+                                    { 2,2,1,2,2,2,2,2 },
+                                    { 2,2,1,0,2,2,2,2 },
+                                    { 2,2,0,1,2,2,2,2 },
+                                    { 2,2,2,2,2,2,2,2 },
+                                    { 2,2,2,2,2,2,2,2 },
+                                    { 2,2,2,2,2,2,2,2 }};
+             */
+            for (int i=0; i<tamTablero; i++){
+                for (int j=0; j<tamTablero; j++){
+                    tablero[i,j]=2;
+                }
+            }
+            int centro=tamTablero/2;
+            tablero[centro-1,centro-1]=1;
+            tablero[centro-1,centro]=0;
+            tablero[centro,centro]=1;
+            tablero[centro,centro-1]=0;
+        }
+            
+        
+
+        /// <summary>
+        /// Función que imprime los valores de la matriz
+        /// </summary>
+        /// <param name="tablero"></param>
+        /// <returns></returns>
+        /// 
+        public static void Imprimir(int[,] tablero ) {
+
+            for (int x = 0; x < tablero.GetLength(0); x++)
+            {
+                for (int y = 0; y < tablero.GetLength(1); y++)
+                {
+                    Console.Write("\t"+tablero[x, y]);
+                }
+                Console.WriteLine();
+            }
+        }
+    
+        
+
+        static void Main(string[] args)
+        {
+            int tipo=0;
+            int tamTablero = 8;
+            int opt=2;
+            RellenarTablero(tamTablero);
+            Imprimir(tablero);
+            Console.ReadKey();
+            Fichas tableroJugada = new Fichas(tablero, tamTablero);
+            
+            //MENU
+            
+            //opcion 1 computadora
+            if(opt==1){
+                for(int i=0; i<tamTablero;i++){
+                    for(int j=0;j<tamTablero;j++){
+                            if(tipo==tablero[i,j]){
+                                Fichas.Comprobacion(i, j,tipo, tamTablero,0);            
+                        }
+                    }
+                }
+            }
+
+            //opcion 2 jugador
+            else if(opt==2){
+            Console.WriteLine("  ");
+                tablero[5,4]=0;
+                Fichas.Comprobacion(5, 4, 0,tamTablero, 3); 
+            }
+            
+            Imprimir(tablero);
+            Console.ReadKey();
+
+        }
+    }
+}
